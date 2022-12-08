4c30f06733fb6d9c06de675b49b2db8c14cb99c6
kesyadewantipongbala <kesya.dewanti@ti.ukdw.ac.id>
2022-12-08 16:04:39 +0700

    sdfgh

---
+++ app/src/main/java/com/example/a71200616/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a71200616
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
